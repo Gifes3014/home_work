97ab8cc76426771f2dd6d319ade677779f2fb2f9
Gifes3014 <108364950+Gifes3014@users.noreply.github.com>
2022-07-19 16:30:08 +0300

    Add files via upload

---
+++ task_13/task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿//          Задача 13
+int a = int.Parse(Console.ReadLine());
+if (a > 99)
+{
+    string b = Convert.ToString(a);
+    Console.WriteLine(b[b.Length - (b.Length - 2)]);
+}
+else
+{
+    Console.WriteLine("нет 3 цифры");
+}
