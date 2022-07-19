af27bfea8202a90de7a8f90134943c425e343901
Gifes3014 <108364950+Gifes3014@users.noreply.github.com>
2022-07-19 16:28:05 +0300

    Add files via upload

---
+++ task_15/task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿//          Задача 15
+int a = int.Parse(Console.ReadLine());
+if ((a >= 1) & (a <= 5))
+{
+    Console.WriteLine("да");
+}
+else if ((a == 6) ^ (a == 7))
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("ошибка");
+}
