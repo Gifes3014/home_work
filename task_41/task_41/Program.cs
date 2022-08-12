70baf4c67c5dac32e71ba59de08a8261142b61e3
Gifes3014 <108364950+Gifes3014@users.noreply.github.com>
2022-08-12 18:34:35 +0300

    Add files via upload

---
+++ task_41/task_41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите колличество чисел которые хотите записать: ");
+int m = int.Parse(Console.ReadLine());
+int GetSum(int n)
+{
+    int r = 0;
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write("Введите число: " + i +" из "+ m+" ");
+        int k = int.Parse(Console.ReadLine());
+        r += k > 0 ? 1 : 0;
+    }
+    return r;
+}
+int s = GetSum(m);
+Console.Write("Кол-во чисел больше 0: "+ s);
---
+++ task_43/task_43/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введдите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введдите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введдите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write("Введдите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+double[] GetXY(double x1, double y1, double x2, double y2)
+{
+    double[] yx = new double[2];
+    yx[0] = (b2 - b1) / (k1 - k2);
+    yx[1] = k2 * yx[0] + b2;
+    return yx;
+}
+double[] xy = GetXY(k1,b1,k2,b2);
+Console.Write(xy[0]+ " "+xy[1]);
