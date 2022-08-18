99e2b0d7e4520153548f378c85e7c1edc39d67f7
Gifes3014 <108364950+Gifes3014@users.noreply.github.com>
2022-08-18 23:54:59 +0300

    Add files via upload

---
+++ task_58/task_58/Program.cs
@@ -0,0 +1,59 @@
+﻿int[,] GetArray(int n, int m, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+int[,] GetArray_2(int n, int m, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+int[,] GetArray_3(int[,] arr, int[,] arr_2, int n, int m)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = arr[i, j] * arr_2[i, j];
+        }
+    }
+    return result;
+}
+void PrintArray_1(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}" + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = GetArray(rows, columns, 0, 9);
+int[,] array_2 = GetArray_2(rows, columns, 0, 9);
+int[,] array_3 = GetArray_3(array, array_2, rows, columns);
+PrintArray_1(array);
+Console.WriteLine();
+PrintArray_1(array_2);
+Console.WriteLine();
+PrintArray_1(array_3);
---
+++ task_60/task_60/Program.cs
@@ -0,0 +1,71 @@
+﻿int[,,] GetArray(int n, int m,int h, int min, int max)
+{
+    int a = 0;
+    int b = 0;
+    int[,,] result = new int[m, n, h];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int f = 0; f < h; f++)
+            {
+                result[i, j, f] = new Random().Next(min, max);
+            }
+            
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int f = 0; f < h; f++)
+            {
+                a = result[i, j, f];
+                for (int q = 0; q < m; q++)
+                {
+                    for (int w = 0; w < n; w++)
+                    {
+                        for (int e = 0; e < h; e++)
+                        {
+                            b = result[q, w, e];
+                            if ((a == b) & (i != q) & (j != w) & (f != e))
+                            {
+                                result[q, w, e] = new Random().Next(min, max);
+                                i = 0;
+                                j = 0;
+                                f = 0;
+                                q = 0;
+                                w = 0;
+                                e = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+    return result;
+}
+void PrintArray(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int f = 0; f < inArray.GetLength(2); f++)
+            {
+                Console.Write(inArray[i, j, f] + " (" + i + j + f + ") ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+Console.Write("Введите z: ");
+int z = int.Parse(Console.ReadLine());
+int[,,] array = GetArray(x, y, z, 10, 99);
+PrintArray(array);
